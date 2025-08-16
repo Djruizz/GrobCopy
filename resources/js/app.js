c97280cf725b9a2d1e53cfea8d072f8a9d6be53a
djruizz <dario.jruiz31@gmail.com>
2025-08-16 16:35:55 -0600

    start views

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
 import routes from "./routes";
-import "../sass/app.scss";
+import "../css/app.scss";
 
 // Router
 const router = createRouter({
